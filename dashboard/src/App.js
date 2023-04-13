23afd25df89188c7fa16ad16d7d49b01b22dd36b
Shilpee-Agrawal <101170944+Shilpee-Agrawal@users.noreply.github.com>
2023-04-13 14:11:49 +0530

    voice recognization

---
+++ dashboard/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import MiniDrawer from './Drawer/Drawer';
+
+function App() {
+  return (
+    <div className="App">
+     <MiniDrawer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ dashboard/src/Drawer/SidebarData.js
@@ -0,0 +1,28 @@
+import DateRangeIconTwoTone from "@mui/icons-material/DateRangeTwoTone";
+import LocalShippingIconTwoTone from "@mui/icons-material/LocalShippingTwoTone";
+import WebhookIconTwoTone from "@mui/icons-material/WebhookTwoTone";
+import WidgetsIconTwoTone from "@mui/icons-material/WidgetsTwoTone";
+import ScienceIconTwoTone from "@mui/icons-material/ScienceTwoTone";
+import HomeIconTwoTone from "@mui/icons-material/HomeTwoTone";
+import DataThresholdingIconTwoTone from "@mui/icons-material/DataThresholdingTwoTone";
+
+export const SidebarData = [
+  {
+    title: "Heim",
+    path: "/user/Produktionseinheit",
+    icons: <HomeIconTwoTone />,
+  },
+  {
+    title: "Ubersicht",
+    path: "/user/Ubersicht",
+    icons: <DateRangeIconTwoTone />,
+  },
+  {
+    title: "Produket",
+    path: "/user/Produket",
+    icons: <WidgetsIconTwoTone />,
+  },
+  
+
+ 
+];
